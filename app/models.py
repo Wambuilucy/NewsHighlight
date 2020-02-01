21f114e912ea46322c48c5dd4c6c0edc3a45c134
Wambuilucy <wambuilucie99@gmail.com>
2020-02-01 13:43:52 +0300

    pushed to heroku

--- app/models.py
+++ app/models.py
@@ -1,5 +1,32 @@
-class Sources:
-    '''
+# class Sources:
+#     '''
+#     Sources class to define Sources Objects
+#     '''
+
+#     def __init__(self,id,name,description,url,category,country,language):
+#         self.id =id
+#         self.name = name
+#         self.description = description
+#         self.url = url
+#         self.category = category
+#         self.country = country
+#         self.language = language
+
+# class Articles:
+#     '''
+#     Articles class to define articles objects
+#     '''
+
+#  def __init__(self,id,author,title,description,url,image,date):
+#         self.id = id
+#         self.author = author
+#         self.title = title
+#         self.description = description
+#         self.url = url
+#         self.image = image
+#         self.date = date
+  class Sources:
+        '''
     Sources class to define Sources Objects
     '''
 
@@ -16,8 +43,7 @@ class Articles:
     '''
     Articles class to define articles objects
     '''
-
- def __init__(self,id,author,title,description,url,image,date):
+    def __init__(self,id,author,title,description,url,image,date):
         self.id = id
         self.author = author
         self.title = title
@@ -25,4 +51,4 @@ class Articles:
         self.url = url
         self.image = image
         self.date = date
-    
+  
